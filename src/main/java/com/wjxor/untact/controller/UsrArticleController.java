609b9f648d4e823c144ce7e299cd4742b951676a
wjxor <shko940575@gmail.com>
2021-07-15 07:23:48 +0900

    사용자 게시물 컨트롤러 추가

---
+++ src/main/java/com/wjxor/untact/controller/UsrArticleController.java
@@ -0,0 +1,36 @@
+package com.wjxor.untact.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wjxor.untact.dto.Article;
+
+@Controller
+public class UsrArticleController {
+	private List<Article> articles;
+	
+	public UsrArticleController() {
+		articles = new ArrayList<>();
+		
+		articles.add(new Article(1, "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(2, "2020-12-12 12:12:12", "제목2", "내용2"));
+	}
+	
+	@RequestMapping("/usr/article/detail")
+	@ResponseBody
+	public Article showDetail(int id) {
+		
+		return articles.get(id - 1);
+	}
+	
+	@RequestMapping("/usr/article/list")
+	@ResponseBody
+	public List<Article> showList() {
+		
+		return articles;
+	}
+}
