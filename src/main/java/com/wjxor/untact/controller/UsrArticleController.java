dc7ef6f456d05b88e340e1a9b1bd0069c52eee5f
wjxor <shko940575@gmail.com>
2021-07-16 04:34:13 +0900

    게시물 삭제

--- src/main/java/com/wjxor/untact/controller/UsrArticleController.java
+++ src/main/java/com/wjxor/untact/controller/UsrArticleController.java
@@ -15,31 +15,31 @@ import com.wjxor.untact.dto.Article;
 public class UsrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
 		// 멤버변수 초기화
 		articlesLastId = 0;
 		articles = new ArrayList<>();
-		
+
 		// 게시물 2개 생성
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
-	
+
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
-		
+
 		return articles.get(id - 1);
 	}
-	
+
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
-		
+
 		return articles;
 	}
-	
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Map<String, Object> doAdd(String regDate, String title, String body) {
@@ -49,7 +49,43 @@ public class UsrArticleController {
 		rs.put("resultCode", "S-1");
 		rs.put("msg", "성공하였습니다.");
 		rs.put("id", articlesLastId);
-		
+
 		return rs;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+
+		Map<String, Object> rs = new HashMap<>();
+		
+		if (deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공하였습니다.");
+		}
+		
+		else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
+		}
+		
+		rs.put("id", id);
+
+		return rs;
+
+	}
+
+	private boolean deleteArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				
+				return true;
+			}
+		}
+		
+		return false;
+		
 	}
 }
